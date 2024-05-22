b03a3114d7efa2d96824475335ff6cc492069434
Annisa Az Zahra <annisa.az@ui.ac.id>
2024-05-22 10:04:46 +0700

    Initial Commit

---
+++ LilyComposer/LilyComposerApp.swift
@@ -0,0 +1,17 @@
+//
+//  LilyComposerApp.swift
+//  LilyComposer
+//
+//  Created by Annisa Az Zahra on 22/05/24.
+//
+
+import SwiftUI
+
+@main
+struct LilyComposerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
